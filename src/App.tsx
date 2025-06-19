7ec35e576819964e37a8db3af546a8fa7cd2868b
amannn-s <s.aman230630@gmail.com>
2025-06-19 08:43:23 +0530

    add loading state and submitting button

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,31 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import DashboardPage from "./pages/DashboardPage";
 import { isAuthenticated } from "./lib/authService";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsUserAuthenticated(isAuthenticated());
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading)
+    return (
+      <section className="w-screen h-screen flex justify-center items-center">
+        <h2 className="text-3xl font-semibold">Loading</h2>
+      </section>
+    );
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path="/"
           element={
-            isAuthenticated() ? (
+            isUserAuthenticated ? (
               <Navigate to="/dashboard" />
             ) : (
               <Navigate to="/login" />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,17 +7,21 @@ import { Button } from "@/components/ui/button";
 export default function Login() {
   const [email, setEmail] = useState("eve.holt@reqres.in");
   const [password, setPassword] = useState("cityslicka");
+  const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+    setSubmitting(true);
     try {
       await login(email, password);
       toast("Logged In Successfully!");
       navigate("/", { replace: true });
     } catch {
       setError("Login failed");
+    } finally {
+      setSubmitting(false);
     }
   };
 
@@ -42,7 +46,10 @@ export default function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
         {error && <div className="text-red-500 text-sm">{error}</div>}
-        <Button className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700">
+        <Button
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+          disabled={submitting}
+        >
           Login
         </Button>
         <Button asChild variant={"secondary"} className="w-full">
